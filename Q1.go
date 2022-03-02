dd6a10f8fe0651eacb605e14d0c95df94b1465cb
unknown <akito030303@gmail.com>
2022-03-02 23:15:36 +0900

    [add]サーバーの復帰時間を表示できるようにした

--- Q1.go
+++ Q1.go
@@ -4,13 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	FileName := "logfile.txt" //開きたいファイルの名前
 
-	f, err := os.Open(FileName)
+	var logData [100][2]string //タイムアウトしているサーバーのタイムスタンプとアドレスを格納する配列
+	serverCount := 0           //タイムアウトしているサーバーの数
+	serverChecker := false     //同じアドレスから連続でタイムアウトが出ていないかの確認するための変数
+	fileName := "logfile.txt"  //開きたいファイルの名前
+
+	f, err := os.Open(fileName)
 	if err != nil { //ファイルをOpenしたときのエラー処理
 		fmt.Println("open error")
 	}
@@ -19,11 +24,35 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
-	/*一行ずつ読み取り、カンマごとに分割し、-が出るとその一つ前の要素(アドレス)をコンソールに出力する。*/
-	for scanner.Scan() {
-		arry1 := strings.Split(scanner.Text(), ",")
-		if arry1[2] == "-" {
-			fmt.Println(arry1[1])
+	/*
+	*一行ずつログファイルを読み取り、カンマごとに分割し、アドレスと復帰時間をコンソールに出力する。
+	 */
+	for scanner.Scan() { //ログファイルを一行ずつ読む
+		arry1 := strings.Split(scanner.Text(), ",") //カンマ毎に分割して、{タイムスタンプ,アドレス,応答時間}として格納している
+
+		for i := 0; i < serverCount; i++ { //同じアドレスから連続でタイムアウトが出ていないかの確認
+			if arry1[1] == logData[i][1] {
+				serverChecker = true
+			}
+		}
+
+		if arry1[2] == "-" && serverChecker == false { //タイムアウトが出たサーバーのタイムスタンプとアドレスをlogData[]に代入している
+			serverCount++
+			logData[serverCount-1][0] = arry1[0]
+			logData[serverCount-1][1] = arry1[1]
+		}
+
+		for i := 0; i < serverCount; i++ {
+			if arry1[1] == logData[i][1] && arry1[2] != "-" { //サーバーが復帰したタイミングでアドレスと故障時間をコンソールに出力する
+				time1, _ := strconv.Atoi(arry1[0])
+				time2, _ := strconv.Atoi(logData[i][0])
+				fmt.Println(logData[i][1])
+				str := fmt.Sprintf("%014d", time1-time2)
+				fmt.Println(str)
+
+				logData[i][0] = "" //タイムアウトしていたサーバーのタイムスタンプの初期化
+				logData[i][1] = "" //タイムアウトしていたサーバーのアドレスの初期化
+			}
 		}
 	}
 
